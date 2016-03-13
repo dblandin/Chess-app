481498e52d8416c509c71566d1a162ebcc073c2e
Harsheek Thanki <harsheekthanki@gmail.com>
2016-03-13 23:19:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamAlphaChess::Application.config.secret_key_base = '1bf485a39f150bafa07f028f76807be2ef33cbcefb6402e22c8f67e45b049add6c28983b5124fd8896c667249907f0121667e85d71087476bb64d11ae3faf070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamAlphaChess::Application.config.session_store :cookie_store, key: '_TeamAlphaChess_session'
