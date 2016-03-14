8242f75200e4aaef98ad14b0c0722e29ff4cb406
Alex Adamo <Shadows@Alexs-MacBook.nyc.rr.com>
2016-03-14 09:33:49 -0400

    -Added rspec gem -Setup Travis Ci file -Added sample test for rspec

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe "StaticPages#index action" do
+    it "should be something here" do
+      expect(true).to be true
+    end
+
+  end
+
+
+end
