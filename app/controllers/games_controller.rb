de7aaf6efbc9263cb3316bdd77891fa63a80a447
Patrick Shushereba <pshushereba@gmail.com>
2016-03-15 21:40:37 +0000

    Working on Games Controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,21 @@ class GamesController < ApplicationController
 	end
 
 	def create
+		@game = Game.create(game_params)
+		redirect_to root_path
 	end
 
 	def show
+		@game = Game.find(params[:id])
 	end
 
 	def update
 	end
 	
+	private
+
+	def game_params
+		params.require(:game).permit(:name)
+	end
+
 end
