593b927ab33e3a17f8c1f6615b3aa8d6eaf8e55e
Patrick Shushereba <pshushereba@gmail.com>
2016-03-17 21:43:47 +0000

    Formatting for Rubocop.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,25 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
 
-	def new
-		@game = Game.new
-	end
+  def create
+    @game = Game.create(game_params)
+    redirect_to root_path
+  end
 
-	def create
-		@game = Game.create(game_params)
-		redirect_to root_path
-	end
+  def show
+    @game = Game.find(params[:id])
+  end
 
-	def show
-		@game = Game.find(params[:id])
-	end
+  def update
+    @game = Game.find(params[:id])
+    @game.update_attributes(game_params)
+  end
 
-	def update
-		@game = Game.find(params[:id])
-		@game.update_attributes(game_params)
-	end
-	
-	private
+  private
 
-	def game_params
-		params.require(:game).permit(:name)
-	end
-
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  describe "StaticPages#index action" do
-    it "should be something here" do
+  describe 'StaticPages#index action' do
+    it 'should be something here' do
       expect(true).to be true
     end
-
   end
-
-
 end
