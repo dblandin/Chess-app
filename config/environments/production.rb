9a620c82401945b5b5ef710efa35df394c2d003c
rosebuddy <helloladybread@gmail.com>
2016-03-17 06:14:04 +0000

    Install bootstrap and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 TeamAlphaChess::Application.configure do
+  config.action_mailer.default_url_options = { host: 'chess-team-alpha-chess.herokuapp.com' }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TeamAlphaChess::Application.routes.draw do
+  devise_for :users
   root 'static_pages#index'
     resources :games
 end
