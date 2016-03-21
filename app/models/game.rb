c82955fd03586c60ccc69019ed77e0d4849ecb9b
Alex Adamo <illuzzi0nz10@gmail.com>
2016-03-21 03:38:55 +0000

    re-added associations I accidently removed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+  belongs_to :white_player, class_name: 'User'
+  belongs_to :black_player, class_name: 'User'
+  belongs_to :winner, class_name: 'User'
+  has_many :pieces
+  
 end
