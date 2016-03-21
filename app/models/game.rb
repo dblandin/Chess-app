ee9cff1dd4268ec970f0bc3c090b659da3f52a43
Alex Adamo <illuzzi0nz10@gmail.com>
2016-03-21 05:05:03 +0000

    Created open_seat? method to check if a game has an opening

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,8 @@ class Game < ActiveRecord::Base
   belongs_to :black_player, class_name: 'User'
   belongs_to :winner, class_name: 'User'
   has_many :pieces
-  
+
+  def open_seat?
+    return true if self.white_player_id == nil or black_player_id == nil
+  end
 end
