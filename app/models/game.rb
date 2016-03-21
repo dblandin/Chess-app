f901fe3001afd2a5f76a8562cde4813d92929664
Alex Adamo <illuzzi0nz10@gmail.com>
2016-03-21 03:26:04 +0000

    added class models for each of the pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,2 @@
 class Game < ActiveRecord::Base
-
-  
-  belongs_to :white_player, class_name: 'User'
-  belongs_to :black_player, class_name: 'User'
-  belongs_to :winner, class_name: 'User'
-  has_many :pieces
-
 end
