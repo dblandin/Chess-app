7708f9c06f6f1403b282274186e839339b984582
Brett Wood <brett_wood@outlook.com>
2016-03-24 23:58:40 +0000

    chessboard created and sidenav animation added. Chessboard is fully responsive

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
 
-  // Initial variables
-
-  var mainSideNavTrigger = $('#mainSideNavTrigger');
-  var mainSideNav = $('#mainSideNav');
-  var mainContentContainer = $('#mainContentContainer');
-  var mainSideNavOpen = false;
-
-  // Listeners
-  mainSideNavTrigger.on('click', function() {
-    if (mainSideNavOpen == false) {
-      mainContentContainer.animate({
-        right: mainSideNav.width
-      }, 'slow', 'linear', function() {
-
-        mainSideNav.fadeIn();
-
-      });
-
-
-    } else {
-
-
-    }
-
-  });
-
 
 });
