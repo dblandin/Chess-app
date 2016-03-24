1065e3c28051bf79da9f4b3868b285423b41f757
Patrick Shushereba <pshushereba@gmail.com>
2016-03-24 03:40:43 +0000

    Added pseudocode for obstructed? method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,14 +5,39 @@ class Piece < ActiveRecord::Base
 
   def obstructed?(destination_row, destination_col)
 
+=begin  * Sample Chessboard *
+            0 1 2 3 4 5 6 7
+            - - - - - - - -
+        0 | 0 1 0 0 0 1 0 0
+        1 | 0 0 0 0 0 0 0 0
+        2 | 0 0 0 0 0 0 0 0
+        3 | 0 0 0 0 0 0 0 0
+        4 | 0 0 0 0 0 0 0 0
+        5 | 0 0 0 0 0 0 0 0
+        6 | 0 0 0 0 0 0 0 0
+        7 | 0 0 0 0 0 0 0 0
+=end
+
+    # determine if we should march forward of backwards
+    # actually march forwards and backwards to build an array of spaces
+    # determine if a piece is in the square from the position.
+
     current_piece_column_index = self.current_column_index
     current_piece_row_index = self.current_row_index
     # Determine number of spaces we need to check
       row_diff = destination_row - current_piece_row_index
       col_diff = destination_col - current_piece_column_index
 
+      delta = something ? 1 : -1
+
     #HORIZONTAL CHECK
     # spaces = []
+
+    # current_x_position = self.current_column_index - delta
+    # while current_x_position != self.current_column_index - delta do
+    #   spaces << [current_x_position, destination_y]
+    #   current_x_position += delta
+    # end
 
     # while col_diff != current_piece_column_index do
     #   spaces.push(curent_piece_row_index,col_diff)
