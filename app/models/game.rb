a87aa95ff30f20a5d7a33f7d73dc8d2e5b73d52d
rosebuddy <helloladybread@gmail.com>
2016-03-25 07:04:50 +0000

    Create test for black and white gamepieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,86 +8,101 @@ class Game < ActiveRecord::Base
 
   def populate_board!
     # this should create all 32 game pieces with their initial x/y coordinates
-
+    # add_pawns
+    # add_rooks
+    # add_knights
+    # add_bishops
+    # add_queen
+    # add_king
+  end
 
     # create white player pieces
 
-    def add_pawns
-    (0..7).each do |i|
-      pieces.create(color: 'white', type: 'Pawn',  current_row_index: 1,
+    def add_pawns_white
+      (0..7).each do |i|
+        pieces.create(color: 'white', type: 'Pawn',  current_row_index: 1,
         current_column_index: i)
+      end
+       # (0..7).each do |i|
+       #  pieces.create(color: 'black', type: 'Pawn', current_row_index: 6,
+       #    current_column_index: i)
+      # end
     end
-  end
   
-    add_pawns
-
-  end
-
-  def add_rooks
-    pieces.create(color: 'white', type: 'Rook', current_row_index: 0, current_column_index: 0)
-    pieces.create(color: 'white', type: 'Rook', current_row_index: 0, current_column_index: 7)
-  end
-
-  def add_knights
-    pieces.create(color: 'white', type: 'Knight', current_row_index: 0, current_column_index: 1)
-    pieces.create(color: 'white', type: 'Knight', current_row_index: 0, current_column_index: 6)
-  end
-
-  def add_bishops
-    pieces.create(color: 'white', type: 'Bishop', current_row_index: 0, current_column_index: 2)
-    pieces.create(color: 'white', type: 'Bishop', current_row_index: 0, current_column_index: 5)
-  end
-
-  
-  def add_queen
-    pieces.create(color: 'white', type: 'Queen', current_row_index: 0, current_column_index: 4)
-  end
-
-
-  def add_king
-    pieces.create(color: 'white', type: 'King', current_row_index: 0, current_column_index: 3)
-  end
-
-# create black player pieces
-    
-         # add_pawns
-  def add_pawns
-    (0..7).each do |i|
-      pieces.create(color: 'black', type: 'Pawn', current_row_index: 6,
-        current_column_index: i)
-    end
-  end
-
-
-
- 
-     # add_rooks
-  def add_rooks
-    pieces.create(color: 'black', type: 'Rook', current_row_index: 7, current_column_index: 0)
-    pieces.create(color: 'black', type: 'Rook', current_row_index: 7, current_column_index: 7)
-  end
    
 
-  def add_knights
-    pieces.create(color: 'black', type: 'Knight', current_row_index: 7, current_column_index: 1)
-    pieces.create(color: 'black', type: 'Knight', current_row_index: 7, current_column_index: 6)
-  end
-
-    # add_bishops
-
-  def add_bishops
-    pieces.create(color: 'black', type: 'Bishop', current_row_index: 7, current_column_index: 2)
-    pieces.create(color: 'black', type: 'Bishop', current_row_index: 7, current_column_index: 5)
-  end
-
-  def add_queen
-    pieces.create(color: 'black', type: 'Queen', current_row_index: 7, current_column_index: 4)
-  end
 
 
-  def add_king
-    pieces.create(color: 'black', type: 'King', current_row_index: 7, current_column_index: 3)
-  end
+    def add_rooks_white
+      pieces.create(color: 'white', type: 'Rook', current_row_index: 0, current_column_index: 0)
+      pieces.create(color: 'white', type: 'Rook', current_row_index: 0, current_column_index: 7)
+    end
+  
+
+    def add_knights_white
+      pieces.create(color: 'white', type: 'Knight', current_row_index: 0, current_column_index: 1)
+      pieces.create(color: 'white', type: 'Knight', current_row_index: 0, current_column_index: 6)
+    end
+    
+
+    def add_bishops_white
+      pieces.create(color: 'white', type: 'Bishop', current_row_index: 0, current_column_index: 2)
+      pieces.create(color: 'white', type: 'Bishop', current_row_index: 0, current_column_index: 5)
+    end
+    
+
+    
+    def add_queen_white
+      pieces.create(color: 'white', type: 'Queen', current_row_index: 0, current_column_index: 4)
+    end
+   
+
+
+    def add_king_white
+      pieces.create(color: 'white', type: 'King', current_row_index: 0, current_column_index: 3)
+    end
+ 
+
+  # create black player pieces
+      
+
+    def add_pawns_black
+      (0..7).each do |i|
+        pieces.create(color: 'black', type: 'Pawn', current_row_index: 6,
+          current_column_index: i)
+      end
+    end
+
+
+
+   
+       # add_rooks
+    def add_rooks_black
+      pieces.create(color: 'black', type: 'Rook', current_row_index: 7, current_column_index: 0)
+      pieces.create(color: 'black', type: 'Rook', current_row_index: 7, current_column_index: 7)
+    end
+     
+
+    def add_knights_black
+      pieces.create(color: 'black', type: 'Knight', current_row_index: 7, current_column_index: 1)
+      pieces.create(color: 'black', type: 'Knight', current_row_index: 7, current_column_index: 6)
+    end
+
+      # add_bishops
+
+    def add_bishops_black
+      pieces.create(color: 'black', type: 'Bishop', current_row_index: 7, current_column_index: 2)
+      pieces.create(color: 'black', type: 'Bishop', current_row_index: 7, current_column_index: 5)
+    end
+
+    def add_queen_black
+      pieces.create(color: 'black', type: 'Queen', current_row_index: 7, current_column_index: 4)
+    end
+
+
+    def add_king_black
+      pieces.create(color: 'black', type: 'King', current_row_index: 7, current_column_index: 3)
+    end
 
 
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 RSpec.describe Game, type: :model do
 
-  describe 'add_pawns' do
-    it 'should create 8 pawns' do
+  describe 'add_pawns_black' do
+    it 'should create 8 black pawns' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_pawns
+      game.add_pawns_black
       expect(game.pieces.count).to eq 8
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['Pawn'])
       expect(game.pieces.pluck(:current_row_index)
         .uniq).to eq([6])
@@ -16,24 +18,58 @@ RSpec.describe Game, type: :model do
     end
   end
 
-  describe 'add_rooks' do
-    it 'should create 2 rooks' do
+  describe 'add_pawns_white' do
+    it 'should create 8 white pawns' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_rooks
+      game.add_pawns_white
+      expect(game.pieces.count).to eq 8
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['Pawn'])
+      expect(game.pieces.pluck(:current_row_index)
+        .uniq).to eq([1])
+      expect(game.pieces.pluck(:current_column_index)
+        .uniq).to eq([0,1,2,3,4,5,6,7])
+    end
+  end
+
+  describe 'add_rooks_black' do
+    it 'should create 2 black rooks' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_rooks_black
       expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['Rook'])
       expect(game.pieces.pluck(:current_row_index).uniq).to eq([7])
       expect(game.pieces.pluck(:current_column_index).uniq).to eq([0,7])
     end
   end
 
-  describe 'add_knights' do
-    it 'should create 2 knights' do
+
+  describe 'add_rooks_white' do
+    it 'should create 2 white rooks' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_knights
+      game.add_rooks_white
       expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['Rook'])
+      expect(game.pieces.pluck(:current_row_index).uniq).to eq([0])
+      expect(game.pieces.pluck(:current_column_index).uniq).to eq([0,7])
+    end
+  end
+  describe 'add_knights_black' do
+    it 'should create 2 black knights' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_knights_black
+      expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['Knight'])
       expect(game.pieces.pluck(:current_row_index).uniq).to eq([7])
       expect(game.pieces.pluck(:current_column_index).uniq).to eq([1,6])
@@ -41,47 +77,103 @@ RSpec.describe Game, type: :model do
     end
   end
 
-  describe 'add_bishops' do
-    it 'should create 2 bishops' do
+  describe 'add_knights_white' do
+    it 'should create 2 white knights' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_bishops
+      game.add_knights_white
       expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['Knight'])
+      expect(game.pieces.pluck(:current_row_index).uniq).to eq([0])
+      expect(game.pieces.pluck(:current_column_index).uniq).to eq([1,6])
+
+    end
+  end
+
+  describe 'add_bishops_black' do
+    it 'should create 2 black bishops' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_bishops_black
+      expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['Bishop'])
       expect(game.pieces.pluck(:current_row_index).uniq).to eq([7])
       expect(game.pieces.pluck(:current_column_index).uniq).to eq([2,5])
-
-
     end
   end
 
-  describe 'add_queen' do
-      it 'should create 1 queen' do
+  describe 'add_bishops_white' do
+    it 'should create 2 white bishops' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_queen
+      game.add_bishops_white
+      expect(game.pieces.count).to eq 2
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['Bishop'])
+      expect(game.pieces.pluck(:current_row_index).uniq).to eq([0])
+      expect(game.pieces.pluck(:current_column_index).uniq).to eq([2,5])
+    end
+  end
+
+  describe 'add_queen_black' do
+      it 'should create 1 black queen' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_queen_black
       expect(game.pieces.count).to eq 1
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['Queen'])
       expect(game.pieces.pluck(:current_row_index).uniq).to eq([7])
       expect(game.pieces.pluck(:current_column_index).uniq).to eq([4])
-
-
     end
-
   end
 
-  describe 'add_king' do
-    it 'should create 1 king' do
+  describe 'add_queen_white' do
+      it 'should create 1 white queen' do
       game = FactoryGirl.create(:game)
       expect(game.pieces.count).to eq 0
-      game.add_king
+      game.add_queen_white
       expect(game.pieces.count).to eq 1
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['Queen'])
+      expect(game.pieces.pluck(:current_row_index).uniq).to eq([0])
+      expect(game.pieces.pluck(:current_column_index).uniq).to eq([4])
+    end
+  end
+
+  describe 'add_king_black' do
+    it 'should create 1 black king' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_king_black
+      expect(game.pieces.count).to eq 1
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['black'])
       expect(game.pieces.pluck(:type).uniq).to eq(['King'])
       expect(game.pieces.pluck(:current_row_index).uniq).to eq([7])
       expect(game.pieces.pluck(:current_column_index).uniq).to eq([3])
-
     end
+  end
 
+  describe 'add_king_white' do
+    it 'should create 1 white king' do
+      game = FactoryGirl.create(:game)
+      expect(game.pieces.count).to eq 0
+      game.add_king_white
+      expect(game.pieces.count).to eq 1
+      expect(game.pieces.pluck(:color)
+        .uniq).to eq(['white'])
+      expect(game.pieces.pluck(:type).uniq).to eq(['King'])
+      expect(game.pieces.pluck(:current_row_index).uniq).to eq([0])
+      expect(game.pieces.pluck(:current_column_index).uniq).to eq([3])
+    end
   end
   
 end
