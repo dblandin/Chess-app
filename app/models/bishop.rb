1ae788c5e28233ad96f91f27beb9f915710be647
Alex Adamo <illuzzi0nz10@gmail.com>
2016-04-06 15:15:16 +0000

    Wrote tests for bishop valid_move?, wrote valid_move? method for bishop, got tests passing

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,12 @@
 class Bishop < Piece
+  def valid_move?(destination_row, destination_col)
+    return false unless diagonal?(destination_row, destination_col)
+    if obstructed?(destination_row, destination_col) || !obstructed?(destination_row, destination_col) && same_color?(destination_row, destination_col)
+      # Move fails
+      false
+    elsif !obstructed?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) || !same_color?(destination_row, destination_col)
+      # Move passes
+      true
+    end
+  end
 end
