8df0ca31897aace412d3535cdda556b51a77fd4e
Alex Adamo <illuzzi0nz10@gmail.com>
2016-04-06 17:17:13 +0000

    Setup more tests, fixed backwards move attempts

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,11 +4,21 @@ class Pawn < Piece
     return false if horizontal?(destination_row, destination_col)
     # Returns false if you try to move forward and the spot is obstructed
     return false if vertical?(destination_row, destination_col) && spot_taken?(destination_row, destination_col)
-    # Returns false if you try to move more then 1 spot
-    return false if distance(destination_row, destination_col) > 1
 
-    #return false if spot_taken?(destination_row, destination_col)
+    if current_row_index == 1 || current_row_index == 6
+      # Returns false if you try to move more then 2 spots when not in the pawn spawn rows
+      return false if distance(destination_row, destination_col) > 2
+    else
+      # Returns false if you try to move more then 1 spot
+      return false if distance(destination_row, destination_col) > 1
+    end
 
+    # Returns false if you try to move backwards
+    if color == 'white'
+      return false if destination_row < current_row_index
+    elsif color == 'black'
+      return false if destination_row > current_row_index
+    end
 
     if diagonal?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
       # Returns false if trying to move diagonal and there is no piece there
@@ -16,14 +26,12 @@ class Pawn < Piece
     elsif vertical?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
       # Returns true if it can move forward one space
       true
+    elsif vertical?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) && distance(destination_row, destination_col) == 2
+      # Returns true if it can move forward two spaces
+      true
     elsif diagonal?(destination_row, destination_col) && spot_taken?(destination_row, destination_col) && !same_color?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
+      # Returns true if it can move diagonal to capture a piece
       true
     end
-
-    #
-    # if
-    #   true
-    # end
-
   end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe Pawn, type: :model do
       expect(white_pawn.valid_move?(3, 2)).to eq false
     end
 
+    it 'should return false if trying to move backwards' do
+      game = FactoryGirl.create(:game)
+      # Select pawn
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
+      white_pawn.update_attributes(current_row_index: 3, current_column_index: 0)
+      expect(white_pawn.valid_move?(2, 0)).to eq false
+    end
+
     it 'should return false if trying to move diagonal with no capture' do
       game = FactoryGirl.create(:game)
       # Select pawn
@@ -35,11 +43,18 @@ RSpec.describe Pawn, type: :model do
       expect(white_pawn.valid_move?(4, 0)).to eq false
     end
 
-    it 'should return true if the first move to move tries to move two spaces forward' do
+    it 'should return true if the first move tries to move two spaces forward(white)' do
       game = FactoryGirl.create(:game)
       # Select pawn
       white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
       expect(white_pawn.valid_move?(3, 0)).to eq true
+    end
+
+    it 'should return true if the first move tries to move two spaces forward(black)' do
+      game = FactoryGirl.create(:game)
+      # Select pawn
+      black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 0)
+      expect(black_pawn.valid_move?(4, 0)).to eq true
     end
 
     it 'should return true if it can move one space forward to an empty spot' do
@@ -57,7 +72,7 @@ RSpec.describe Pawn, type: :model do
       black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 0)
       # Move black pawn to be in the way
       black_pawn.update_attributes(current_row_index: 2, current_column_index: 1)
-      expect(white_pawn.valid_move?(2,1)).to eq true
+      expect(white_pawn.valid_move?(2, 1)).to eq true
     end
   end
 end
