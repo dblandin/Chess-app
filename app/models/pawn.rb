8c72c9727b4191fc7b3561ef654fe125d25edff3
Alex Adamo <illuzzi0nz10@gmail.com>
2016-04-06 16:22:51 +0000

    Added more tests, fleshed out some of the valid_move? method workings for pawn.rb

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,29 @@
 class Pawn < Piece
+  def valid_move?(destination_row, destination_col)
+    # Dont allow horizontal moves
+    return false if horizontal?(destination_row, destination_col)
+    # Returns false if you try to move forward and the spot is obstructed
+    return false if vertical?(destination_row, destination_col) && spot_taken?(destination_row, destination_col)
+    # Returns false if you try to move more then 1 spot
+    return false if distance(destination_row, destination_col) > 1
+
+    #return false if spot_taken?(destination_row, destination_col)
+
+
+    if diagonal?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
+      # Returns false if trying to move diagonal and there is no piece there
+      false
+    elsif vertical?(destination_row, destination_col) && !spot_taken?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
+      # Returns true if it can move forward one space
+      true
+    elsif diagonal?(destination_row, destination_col) && spot_taken?(destination_row, destination_col) && !same_color?(destination_row, destination_col) && distance(destination_row, destination_col) == 1
+      true
+    end
+
+    #
+    # if
+    #   true
+    # end
+
+  end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,29 +1,22 @@
 require 'rails_helper'
 RSpec.describe Pawn, type: :model do
   describe 'valid_move?' do
-    it 'should return true to allow the first move to move two spaces' do
+    it 'should return false if trying to move horizontal' do
       game = FactoryGirl.create(:game)
       # Select pawn
       white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
-      expect(white_pawn.valid_move?(3,0)).to eq true
+      white_pawn.update_attributes(current_row_index: 3, current_column_index: 1)
+      expect(white_pawn.valid_move?(3, 2)).to eq false
     end
 
-    it 'should return false if trying to move two spaces on anymove but the first' do
+    it 'should return false if trying to move diagonal with no capture' do
       game = FactoryGirl.create(:game)
       # Select pawn
       white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
-      white_pawn.update_attributes(current_row_index: 2, current_column_index: 0)
-      expect(white_pawn.valid_move?(4,0)).to eq false
+      expect(white_pawn.valid_move?(2, 1)).to eq false
     end
 
-    it 'should return true if it can move one space forward to an empty spot' do
-      game = FactoryGirl.create(:game)
-      # Select pawn
-      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
-      expect(white_pawn.valid_move?(2,0)).to eq true
-    end
-
-    it 'should return false if it tries to move forward into a piece' do
+    it 'should return false if it tries to move forward and its blocked' do
       game = FactoryGirl.create(:game)
       # Select pawn
       white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
@@ -31,7 +24,29 @@ RSpec.describe Pawn, type: :model do
       black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 0)
       # Move black pawn to be in the way
       black_pawn.update_attributes(current_row_index: 2, current_column_index: 0)
-      expect(white_pawn.valid_move?(2,0)).to eq false
+      expect(white_pawn.valid_move?(2, 0)).to eq false
+    end
+
+    it 'should return false if trying to move two spaces on anymove but the first' do
+      game = FactoryGirl.create(:game)
+      # Select pawn
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
+      white_pawn.update_attributes(current_row_index: 2, current_column_index: 0)
+      expect(white_pawn.valid_move?(4, 0)).to eq false
+    end
+
+    it 'should return true if the first move to move tries to move two spaces forward' do
+      game = FactoryGirl.create(:game)
+      # Select pawn
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
+      expect(white_pawn.valid_move?(3, 0)).to eq true
+    end
+
+    it 'should return true if it can move one space forward to an empty spot' do
+      game = FactoryGirl.create(:game)
+      # Select pawn
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 0)
+      expect(white_pawn.valid_move?(2, 0)).to eq true
     end
 
     it 'should return true for this capture' do
