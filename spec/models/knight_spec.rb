fce40c9fc608f865a2de6885157f9725a3a8fec9
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-08 20:28:37 +0000

    Clean up knight_spec.rb for rubocop

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Knight, type: :model do
     it 'should return true for valid_move if theres an empty destination spot' do
       game = FactoryGirl.create(:game)
       # Select white knight from the board
-      white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0,1)
+      white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0, 1)
       # Should return true as it is a valid move and destination spot is empty
       expect(white_knight.valid_move?(2, 2)).to eq true
     end
@@ -12,11 +12,11 @@ RSpec.describe Knight, type: :model do
     it 'should return false for valid_move if theres a piece with same color' do
       game = FactoryGirl.create(:game)
       # Select white knight from the board
-      white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0,1)
+      white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0, 1)
       # Select white pawn from the board
-      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1,3)
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 3)
       # Should return false as there is an existing same color piece in the destination square
-      expect(white_knight.valid_move?(1,3)).to eq false
+      expect(white_knight.valid_move?(1, 3)).to eq false
     end
 
     it 'should return false for valid_move if piece move is invalid' do
@@ -24,7 +24,7 @@ RSpec.describe Knight, type: :model do
       # Select white knight from the board
       white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0, 1)
       # Should return false as a knight cannot move to one adjacent square
-      expect(white_knight.valid_move?(0, 2)).to eq false  
+      expect(white_knight.valid_move?(0, 2)).to eq false
     end
 
     it 'should return true for valid_move if theres a piece with different color' do
@@ -32,15 +32,15 @@ RSpec.describe Knight, type: :model do
       # Select white knight from the board
       white_knight = game.pieces.find_by_current_row_index_and_current_column_index(0, 1)
       # Select white pawn from the board
-      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1,3)
+      white_pawn = game.pieces.find_by_current_row_index_and_current_column_index(1, 3)
       # Select black pawn from the board
-      black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6,3)
+      black_pawn = game.pieces.find_by_current_row_index_and_current_column_index(6, 3)
       # Remove white_pawn from board
       white_pawn.update_attributes(current_row_index: nil, current_column_index: nil)
-      # Move black_pawn into (1,3) spot
+      # Move black_pawn into (1, 3) spot
       black_pawn.update_attributes(current_row_index: 1, current_column_index: 3)
       # Should return true as it is a valid move and there is a piece with different color in destination spot
-      expect(white_knight.valid_move?(1,3)). to eq true
+      expect(white_knight.valid_move?(1, 3)).to eq true
     end
   end
 end
