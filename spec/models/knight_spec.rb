8412ee1634e8572b7ec4edff470a793e78ae1bea
Harsheek Thanki <harsheekthanki@gmail.com>
2016-04-08 18:30:27 +0000

    Add test descriptions for valid_move? method in knight_spec.rb

---
+++ spec/models/knight_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.describe Knight, type: :model do
+  describe 'valid_move?' do
+    it 'should return true for valid_move if theres an empty destination spot' do
+    end
+
+    it 'should return false for valid_move if theres a piece with same color' do
+    end
+
+    it 'should return false for valid_move if piece move is invalid' do
+    end
+
+    it 'should return true for valid_move if theres a piece with different color' do
+    end
+  end
+end
